421035451ad0f158013819f18fbb348b8e315522
Md Adil Lam <31787784+Adilalaminfo@users.noreply.github.com>
2021-08-20 22:21:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <>
+  
+  <h1>Netflix Movies</h1>
+  <p>Netflix top five Movies</p>
+  <ol type="1">
+  <li>ABCD</li>
+  <li>Shershah</li>
+  <li>Bhuj</li>
+  <li>Harry Poter</li>
+  <li>Conujuring</li>
+</ol>
+  </>,
+    document.getElementById('root')
+);
+
